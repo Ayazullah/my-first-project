2af3568ea0d664f52642f01576b6e09a2e5801fc
Ayazullah <69949570+Ayazullah@users.noreply.github.com>
2020-09-07 11:58:26 +0530

    Add files via upload

---
+++ Basha.java
@@ -0,0 +1,9 @@
+ // Your First Program
+
+class I'm ayazullah, I'm from palamaner {
+
+    public static void main(String[] args) {
+        // Prints "Hello World, DevOps has arrived!" to the terminal window.
+        System.out.println("Hello World, DevOps has arrived!");
+    }
+}
